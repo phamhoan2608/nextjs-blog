76fdc2ea42497859c0883d96561424307edde40d
Pham Van Hoan <phamhoan879@gmail.com>
2023-05-19 15:06:37 +0700

    Change description meta

--- components/layout.tsx
+++ components/layout.tsx
@@ -17,10 +17,7 @@ export default function Layout({ children, home }: LayoutProps): JSX.Element {
     <div className={styles.container}>
       <Head>
         <link rel="icon" href="/favicon.ico" />
-        <meta
-          name="description"
-          content="Learn how to build a personal website using Next.js"
-        />
+        <meta name="description" content="Ha Anh Blogger" />
         <meta
           property="og:image"
           content={`https://og-image.vercel.app/${encodeURI(
